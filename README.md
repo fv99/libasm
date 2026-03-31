228fccb8ef5df992edc3ab517808d7306e08237c
fv99 <filipvon@protonmail.com>
2026-03-31 13:52:46 +0200

    hello world

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # libasm
-42 assembly project
+42 x86-64 assembly project
+
+**Useful links**
+```
+https://linux-kernel-labs.github.io/refs/heads/master/lectures/syscalls.html
+https://www.chromium.org/chromium-os/developer-library/reference/linux-constants/syscalls/
+https://www.nasm.us/doc/nasm03.html
+https://cs61.seas.harvard.edu/site/2025/Asm/
+https://github.com/mschwartz/assembly-tutorial
+```
--- hello_world.s
+++ hello_world.s
@@ -1,18 +1,18 @@
-global _start
+global _start ; for linker
 
-section .text
+section .text ; executable
 
 _start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, world!\n",
-  mov rdx, msglen   ;   sizeof("Hello, world!\n")
-  syscall           ; );
+  mov rax, 1        ; syscall 1 = write
+  mov rdi, 1        ; first arg - file descriptor 1 - STDOUT,
+  mov rsi, msg      ; second arg - pointer to our string msg
+  mov rdx, msglen   ; 14 bytes - len of "Hello, world!\n"
+  syscall           ; write 14 bytes to stdout
 
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
+  mov rax, 60       ; syscall 60 = exit
+  mov rdi, 0        ; sets arg to code 0 EXIT_SUCCESS
+  syscall           ; kernel exits
 
-section .rodata
-  msg: db "Hello, world!", 10
-  msglen: equ $ - msg
+section .rodata ; read only data (nasm instructions)
+  msg: db "Hello, world!", 10 ; db = define bytes, 10 = ascii newline
+  msglen: equ $ - msg ; equ = #define in C, $ = current position - msg = length
